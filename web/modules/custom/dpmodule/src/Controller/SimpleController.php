92fee2834ddf2e860ab87e3dd13559845867e938
innoDebrup <debrup.pramanik@innoraft.com>
2024-07-26 09:46:25 +0530

    FT2-233: Added Comments for better code readability.

--- web/modules/custom/dpmodule/src/Controller/SimpleController.php
+++ web/modules/custom/dpmodule/src/Controller/SimpleController.php
@@ -4,13 +4,30 @@ namespace Drupal\dpmodule\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
 
+/**
+ *  Class SimpleController for managing dpmodule functionality.
+ */
 class SimpleController extends ControllerBase {
+  
+  /**
+   * Current User of the site.
+   *
+   * @var AccountInterface
+   */
   protected $currentUser;
 
+  /**
+   * Constructor of the Controller class.
+   *
+   * @param AccountInterface $current_user
+   */
   public function __construct(AccountInterface $current_user) {
     $this->currentUser = $current_user;
   }
 
+  /**
+   * Function to greet currently logged-in user.
+   */
   public function displayUsername() {
     $username = $this->currentUser->getAccountName();
     return [
